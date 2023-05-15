bfc08b11e4ec858f616c7358bcccdaa067f8080f
nuci <nuci@gmail.com>
2023-05-15 21:10:49 +0400

    add banner section home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './Page/Home'
+function App() {
+
+  return (
+    <>
+      <div className='mainContainer'>
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
